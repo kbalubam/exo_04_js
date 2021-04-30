6ec1adce99e81484d2f96e8c42079c56c3c07942
kbalubam <kevin.baluba@hotmail.com>
2021-04-30 14:42:12 +0200

    majuscule

---
+++ src/js/main.js
@@ -0,0 +1,26 @@
+// exo1
+let question = prompt("Quel est ton prénom");
+alert("Bonjour " + question);
+
+//exo 2
+let age = prompt("Quel age as tu?");
+alert("Tu as " + age);
+
+// exo3
+let firstnameUser = prompt("Quel est ton prénom");
+let nameUser = prompt("quel est ton nom de famille?");
+alert("ton prénom est " + firstnameUser + " et ton nom de famille est " + nameUser);
+
+//exo4
+
+let firstnameUser2 = prompt("Quel est ton prénom");
+let nameUser2 = prompt("Quel est ton prénom");
+ firstnameUser2 = firstnameUser2.toUpperCase();
+ nameUser2 = nameUser2.toUpperCase();
+ alert("ton prénom est " + firstnameUser2 + " et ton nom de famille est " + nameUser2);
+
+
+
+
+
+
